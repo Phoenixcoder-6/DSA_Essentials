ee2f2557d1ad083c0fd0f0f20df3d0583535acb7
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2025-04-06 12:42:24 +0530

    Add files via upload

---
+++ Array/Find Duplicate.py
@@ -0,0 +1,16 @@
+def duplicate(arr):
+    freq={}
+    n= len(arr)
+    for i in arr:
+        if i in freq:
+            freq[i]+=1
+        else:
+            freq[i]=1
+
+
+    for i, val in freq.items():
+        if val>1:
+            print(f"Duplicate present. Duplicate element is {i}")
+    
+arr=[2,5,4,3,2,1,2,1]
+duplicate(arr)
