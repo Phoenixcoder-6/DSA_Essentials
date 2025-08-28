99e23bab770f55c03a46e01dfaad7f983a8f9e42
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2025-08-28 14:11:59 +0530

    Add files via upload

---
+++ Array/Armstrong_number.py
@@ -0,0 +1,14 @@
+def armstrong_number(num):
+    n=len(str(num))
+    total=0
+    for i in str(num):
+        total += int(i) ** n
+
+    if total == num:
+        return True
+    return False
+
+
+num=9474
+result= armstrong_number(num)
+print(result)
